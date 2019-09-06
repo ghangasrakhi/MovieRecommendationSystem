b129e1082f1cb5d65257191dfd8b2942596f19ec
Rakhi Ghangas <ghangas.rakhi@gmail.com>
2019-09-07 09:41:15 +1000

    first commit

---
+++ Movierecon.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  5 17:22:52 2019
+
+@author: USER
+"""
+
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+text = ["London Paris London","Paris Paris London"]
+cv= CountVectorizer()
+
+count_matrix = cv.fit_transform(text)
+
+#print(count_matrix.toarray())
+
+similarity_scores =cosine_similarity(count_matrix)
+print(similarity_scores)
